73f13fc396425208523cf4d7c0bfdc45ce252f12
James Thomas <jabt1998@gmail.com>
2021-04-27 09:41:24 +0100

    created docs

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <h2>Brewdog</h2>
+    <Navbar />
+  );
+}
+
+export default App;
